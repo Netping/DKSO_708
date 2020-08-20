5fe404fb34e25ef9b0658c2b9a0025ce19e0a8b2
smike-ru <sadmix@yandex.ru>
2020-08-20 12:27:36 -0700

    feat(packages): add bridge-utils, quagga, minicom, stress, htop, example1, mmc-utils packages

---
+++ package/utils/example1/src/main.c
@@ -0,0 +1,35 @@
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+
+int main(int argc, char **argv)
+{
+	int secs = 20;
+
+	if (argc == 2) {
+		secs = atoi(argv[1]);
+	}
+
+	printf("Hi, this is example1!\n");
+	printf("Forking into background now and exit after %d seconds.\n", secs);
+
+	pid_t result = fork();
+
+	if (result == -1)
+	{
+		fprintf(stderr, "Failed to fork: %s.", strerror(errno));
+		return 1;
+	}
+	else if (result == 0)
+	{
+		// Child process sleeps some seconds before exit.
+		sleep(secs);
+	}
+
+	return 0;
+}
